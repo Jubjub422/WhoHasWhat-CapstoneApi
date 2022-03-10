13a11cf30c76f3588b723531c0eaed20cfafb152
Jubjub422 <ross.stovenour@gmail.com>
2022-03-10 16:00:38 -0600

    fixtures and tables added

--- whohaswhatapi/models/__init__.py
+++ whohaswhatapi/models/__init__.py
@@ -3,4 +3,5 @@ from .Condition import Condition
 from .Item import Item
 from .Lender import Lender
 from .Rating import Rating
-from RentedItem import RentedItem
+from .RentedItem import RentedItem
+from .ItemCategory import ItemCategory
